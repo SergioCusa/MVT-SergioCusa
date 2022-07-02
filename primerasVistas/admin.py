bb9e76e053d8b774a63e00574ffcf66f9a5a029b
Sergio Cusa <sergiocusa.91@gmail.com>
2022-07-02 09:39:18 -0300

    agregue boton con vinculo

--- primerasVistas/admin.py
+++ primerasVistas/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from django.db import models
 
 # Register your models here.
+
+class Persona (models.Model):
+    nombre=models.CharField(max_length=30)
+    edad=models.IntegerField()
+    fecha=models.DateField(null=True)
+     
